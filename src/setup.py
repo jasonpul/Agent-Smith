ae3724a6a41c65e1193d61e057567ab32c81c7bb
jasonpul <jason.pul.ca@gmail.com>
2021-07-28 16:24:47 -0400

    added download link to setup.py

--- src/setup.py
+++ src/setup.py
@@ -7,5 +7,7 @@ setup(
     description='Python Reinforcement Learning Library with PyTorch',
     author='Jason Pul',
     license='MIT',
+    download_url='https://github.com/jasonpul/Agent-Smith/releases/download/0.1.0/Agent-Smith-0.1.0.tar.gz',
+    keywords=['pytorch', 'dqn', 'a2c', 'reinforcement'],
     install_requires=['torch==1.9.0', 'plotly==5.1.0', 'matplotlib==3.4.2'],
 )
