6733a593e06bdb1b0c01330bb5e11ee839a020bb
jasonpul <jason.pul.ca@gmail.com>
2021-07-28 17:06:22 -0400

    removed plotly requirement and used direct link to packages in setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,12 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='Agent-Smith',
-    packages=find_packages(include=['AgentSmith']),
     version='0.1.0',
     description='Python Reinforcement Learning Library with PyTorch',
+    url='https://github.com/jasonpul/Agent-Smith',
     author='Jason Pul',
     license='MIT',
-    download_url='https://github.com/jasonpul/Agent-Smith/releases/download/0.1.0/Agent-Smith-0.1.0.tar.gz',
+    packages=['AgentSmith'],
+    include_package_data=True,
+    install_requires=['torch==1.9.0', 'matplotlib==3.4.2'],
     keywords=['pytorch', 'dqn', 'a2c', 'reinforcement'],
-    install_requires=['torch==1.9.0', 'plotly==5.1.0', 'matplotlib==3.4.2'],
 )
